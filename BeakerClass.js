282b0e93e20923f7d8e7252bc6aa300578cdd6bc
Richard Hayes <rhshadowgreen@gmail.com>
2020-02-21 16:27:46 -0500

    Working color mixing

--- BeakerClass.js
+++ BeakerClass.js
@@ -1,39 +1,174 @@
 class Beaker {
-    constructor() {
+    constructor(color) {
         // beakerContainer
-        let bc = document.createElement("div");
-        bc.className = "beaker-container";
+        this.bc = document.createElement("div");
+        this.bc.className = "beaker-container";
         // beakerClickZone
-        let bcz = document.createElement("div");
-        bcz.className = "beaker-click-zone";
+        this.bcz = document.createElement("div");
+        this.bcz.className = "beaker-click-zone";
         // beakerLiquidZone
-        let blz = document.createElement("div");
-        blz.className = "beaker-liquid-zone";
+        this.blz = document.createElement("div");
+        this.blz.className = "beaker-liquid-zone";
         // SVG
         var parser = new DOMParser();
-        var doc = parser.parseFromString(this.getSVGString(), "image/svg+xml");
-        let svg = doc.documentElement;
-        bc.appendChild(bcz);
-        bc.appendChild(blz);
-        bc.appendChild(svg);
-        this._ref = bc;
+        var doc = parser.parseFromString(this.getSVGString(global.uid()), "image/svg+xml");
+        this.svg = doc.documentElement;
+        this.bc.appendChild(this.bcz);
+        this.bc.appendChild(this.blz);
+        this.bc.appendChild(this.svg);
+        this._ref = this.bc;
+
+        // Base color
+        this._color = color;
+        if (color == undefined) {
+            this._color = "#FF0000";
+        }
+        this.color(this._color);
+
+        // Set click events
+        this.bcz.addEventListener("mousedown", e => {
+            if (global.movementLock) return; // Prevent events during animation
+            if (e.shiftKey) {
+                this.pourEvent();
+            } else {
+                global.target = e.target.parentElement;
+
+                let left = 0;
+                if (e.target.parentElement.style.left.length != 0) {
+                    left = this.pxToInt(e.target.parentElement.style.left);
+                }
+                let top = 0;
+                if (e.target.parentElement.style.top.length != 0) {
+                    top = this.pxToInt(e.target.parentElement.style.top);
+                }
+                global.clickPosition = {
+                    "left": left,
+                    "top": top
+                };
+                global.clickOffset = {
+                    "left": e.clientX - left,
+                    "top": e.clientY - top
+                };
+            }
+        });
+        this.bcz.addEventListener("mouseup", e => {
+            global.target = undefined;
+        });
+        global.beakers.push(this);
     }
     color(hex) {
         // Set new hex color for liquid in beaker
+        this._color = hex;
+        this.svg.querySelector("#liquid").setAttribute("fill", hex);
+        this.svg.querySelector("#liquidDrop").setAttribute("fill", hex);
     }
     ref() {
         // Return ref to beaker DOM element
         return this._ref;
     }
-    getSVGString() {
+    pxToInt(px) {
+        // Accepts px as "5px" returns number value ex. 5
+        return parseInt(px.substring(0, px.length - 2));
+    }
+    mix(hex) {
+        // Accept hex as new color to mix with current
+        let other = this.seperateComponents(hex);
+        let self = this.seperateComponents(this._color);
+
+        let r = self.r + other.r;
+        let g = self.g + other.g;
+        let b = self.b + other.b;
+
+        // Get max
+        let max = parseFloat(Math.max(r, g, b));
+
+        // Normailze color channels
+        let ratio = max / 255.0;
+        let recip = 1 / ratio;
+        r = Math.floor(r * recip);
+        g = Math.floor(g * recip);
+        b = Math.floor(b * recip);
+
+        let newColor = this.createHexFromComp({
+            "r": r,
+            "g": g,
+            "b": b
+        });
+
+        this.color(newColor);
+    }
+    seperateComponents(hex) {
+        // Accepts hex as "#FF00FF"
+        // Returns object with rgb components as base ten integers
+        return {
+            "r": parseInt(hex[1] + hex[2], 16),
+            "g": parseInt(hex[3] + hex[4], 16),
+            "b": parseInt(hex[5] + hex[6], 16)
+        };
+    }
+    createHexFromComp(comp) {
+        let r = this.pad(comp.r.toString(16));
+        let g = this.pad(comp.g.toString(16));
+        let b = this.pad(comp.b.toString(16));
+        return "#" + r + g + b;
+    }
+    pad(str) {
+        if (str.length == 0) {
+            return "00";
+        }
+        if (str.length == 1) {
+            return "0" + str;
+        }
+        return str;
+    }
+    pourEvent() {
+        global.movementLock = true;
+
+        // Trigger animation
+        let je = this.svg.querySelector("#jarEdge");
+        let mt = this.svg.querySelector("#measureTicks");
+        let la = this.svg.querySelector("#liquidClipArea");
+        let ld = this.svg.querySelector("#liquidDrop");
+
+        let elements = [je, mt, la];
+        for (let elm of elements) {
+            elm.setAttribute("class", "tip");
+            var newone = elm.cloneNode(true);
+            elm.parentNode.replaceChild(newone, elm);
+        }
+        ld.setAttribute("class", "drop");
+        var newone = ld.cloneNode(true);
+        ld.parentNode.replaceChild(newone, ld);
+
+        setTimeout(() => {
+            let left = 0;
+            if (this.bc.style.left.length != 0) {
+                left = this.pxToInt(this.bc.style.left);
+            }
+            let top = 0;
+            if (this.bc.style.top.length != 0) {
+                top = this.pxToInt(this.bc.style.top);
+            }
+            // Trigger global pour event
+            global.pourEvent(this._color, {
+                "left": left + 154, // container.left + svg.liquidDrop.x
+                "top": top + 110 + 150 // container.top + svg.liquidDrop.y + anim.height
+            });
+        }, 2000);
+
+        setTimeout(() => {
+            global.movementLock = false;
+        }, 3500);
+    }
+    getSVGString(id) {
         return `<svg width="200" height="400" viewBox="0 0 200 200" preserveAspectRatio="xMidYMid meet" fill="none" xmlns="http://www.w3.org/2000/svg">
             <defs>
-              <clipPath id="clip">
+              <clipPath id="clip${id}">
                 <path id="liquidClipArea" d="M64.8456 142.947L64.8456 92.4466V49.9466L130.846 49.9466V92.4466V142.947C130.346 149.947 128.346 150.447 121.346 150.447H76.3456C67.3456 150.447 64.8456 147.947 64.8456 142.947Z" fill="#FEBA94" fill-opacity="0.5"/>
               </clipPath>
             </defs>
             <rect id="background" x="1.14441e-05" width="200" height="200" fill="#D0D0D0" opacity="0"/>
-            <rect id="liquid" x="35" y="110" width="135" height="53" fill="#67B9DD" clip-path="url(#clip)"/>
+            <rect id="liquid" x="35" y="110" width="135" height="53" fill="#67B9DD" clip-path="url(#clip${id})"/>
             <rect id="liquidDrop" x="154" y="110" width="6" height="7" rx="3" fill="#67B9DD"/>
             <path id="jarEdge" d="M47.9454 55.3878C44.4454 52.3878 44.9454 48.8878 51.4454 48.8878L144.445 48.8878C150.945 48.8878 150.945 52.3878 147.945 55.3878C147.945 55.3878 137.945 64.3878 137.945 71.3878V139.888C137.945 153.888 126.445 157.888 120.445 157.888H75.4454C65.4454 157.888 58.4454 150.888 57.9454 139.888L57.9454 71.3878C57.9454 64.3878 47.9454 55.3878 47.9454 55.3878Z" stroke="#F9FEFF" stroke-width="2"/>
             <g id="measureTicks">
--- script.js
+++ script.js
@@ -1,9 +1,86 @@
+let global = {
+    "target": undefined,
+    "clickPosition": { // Position of beaker svg on click
+        "left": 0, "top": 0
+    },
+    "clickOffset": { // Click offset inside svg container
+        "left": 0, "top": 0
+    },
+    "movementLock": false,
+    "uid": (() => { // Returns new uid when called as function
+        let uid = 0;
+        return () => {
+            uid += 1;
+            return uid;
+        }
+    })(),
+    "pourEvent": (hex, targetPosition) => {
+        pourEvent(hex, targetPosition)
+    },
+    "beakers": []
+};
+
+function pourEvent(hex, targetPosition) {
+    // Accepts hex and target position of pouring container
+    // Triggers any effected containers to change
+    for (let beaker of global.beakers) {
+        let container = beaker.ref();
+        // Container position
+        let cx = pxToInt(container.style.left);
+        let cy = pxToInt(container.style.top);
+
+        // Liquid bounding rect
+        let topLeftX = cx + 64; // container.x + liquid.x(estimate from path)
+        let topLeftY = cy + 110; // container.y + liquid.y
+        let boundHeight = 45;
+        let boundWidth  = 65;
+
+        let tx = targetPosition.left;
+        let ty = targetPosition.top;
+
+        // Check bounds
+        let one = (tx >= topLeftX) && (tx <= topLeftX + boundWidth);
+        let two = (ty >= topLeftY) && (ty <= topLeftY + boundHeight);
+        if (one && two) {
+            beaker.mix(hex);
+        }
+    }
+}
+
+function pxToInt(px) {
+    // Accepts px as "5px" returns number value ex. 5
+    return parseInt(px.substring(0, px.length - 2));
+}
+
 function main() {
     let pageArea = document.querySelector(".page-area");
 
-    let beaker = new Beaker();
+    let beaker = new Beaker("#FF0000");
     pageArea.appendChild(beaker.ref());
 
-    
+    let beaker2 = new Beaker("#00FF00");
+    pageArea.appendChild(beaker2.ref());
+
+    // Move target on mouse move
+    window.addEventListener("mousemove", e => {
+        if (global.target != undefined) {
+            // Current position of mouse in window
+            let cx = e.clientX;
+            let cy = e.clientY;
+
+            // Delta between start and current
+            let dx = cx - global.clickPosition.left;
+            let dy = cy - global.clickPosition.top;
+
+            // New position for beaker
+            let nx = global.clickPosition.left + dx - global.clickOffset.left;
+            let ny = global.clickPosition.top + dy - global.clickOffset.top;
+
+            // Assign new position
+            global.target.style.left = nx + "px";
+            global.target.style.top = ny + "px";
+        }
+    });
+
 }
 window.onload = main;
